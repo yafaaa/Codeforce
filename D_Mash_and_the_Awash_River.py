797a4b9736f60fa37db0d9ca739cd02c68314239
yafaaa <yafetmichael36@gmail.com>
2026-02-08 18:32:38 +0300

    mash and the awash river

--- D_Mash_and_the_Awash_River.py
+++ D_Mash_and_the_Awash_River.py
@@ -2,13 +2,24 @@
 for _ in range(int(input())):
     string = input()
     n = len(string)
-    s = 0
-    for a in string:
-        if a == '<':
-            s-=1
-        elif a == '>':
-            s+=1
-    if s==0:
+    if "><" in string or "*<" in string or ">*" in string or "**" in string:
         print(-1)
-    else:
-        print(abs(s))
+        continue
+    if string[0]=="*" or string[-1]=="*":
+        print(n)
+        continue
+    m = 1
+    prev = string[0]
+    for i in range(1,n):
+        ch = string[i]
+        if ch != prev:
+            if ch == "*":
+                m = max(n-(i+1), i)+1
+            else:
+                m = max(n-(i), i)
+            break
+        m+=1
+    print(m)
+        
+
+

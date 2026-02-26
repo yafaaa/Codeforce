bae3ef4dfb983b4e4e7ccdcdd9c52dcbf35694e8
yafaaa <yafetmichael36@gmail.com>
2026-02-26 18:44:21 +0300

    1709 finially

--- D_1709.py
+++ D_1709.py
@@ -1,48 +1,30 @@
 
 for _ in range(int(input())):
     n = int(input())
-    l1 = list(map(int, input().split()))
+    l1 = list(map(int, input().split())) 
     l2 = list(map(int, input().split()))
-
     res = []
+
+    for i in range(n):
+        if l2[i] < l1[i]:
+            l1[i], l2[i] = l2[i], l1[i]
+            res.append([3, i+1])
     
-    for val in range(1, n+1):
-        if val in l1:
-            idx = l1.index(val)
-            for i in range(idx-1, val-2, -1): #val-1 is index
-                l1[i], l1[i+1] = l1[i+1], l1[i]
-                res.append([1, i+1])
-        else:
-            idx = l2.index(val)
-            if val-1 <= idx:
-                for i in range(idx-1, val-2, -1): #val-1 is index
-                    l2[i], l2[i+1] = l2[i+1], l2[i]
-                    res.append([2, i+1])
+    
+    for i in range(n-1, 0, -1):
+        for j in range(i):
+            if l1[j+1] < l1[j]:
+                l1[j], l1[j+1] = l1[j+1], l1[j]
+                res.append([1, j+1])
+    l1 = l2
+    
+    for i in range(n-1, 0, -1):
+        for j in range(i):
+            if l1[j+1] < l1[j]:
+                l1[j], l1[j+1] = l1[j+1], l1[j]
+                res.append([2, j+1])
         
-            else:
-                for i in range(idx, val-1):
-                    l2[i], l2[i+1] = l2[i+1], l2[i]
-                    res.append([2, i+1])
-            
-            l1[val-1], l2[val-1] = l2[val-1], l1[val-1]
-            res.append([3, val-1+1])
-       
-
     
-    for val in range(n+1, 2*n+1): # val-(n+1) is val-1 here
-        idx = l2.index(val)
-        if val-(n+1) <= idx:
-            for i in range(idx-1, val-(n+1)-1, -1): 
-                l2[i], l2[i+1] = l2[i+1], l2[i]
-                res.append([2, i+1])        
-            
-        else:
-            for i in range(idx, val-(n+1)):
-                l2[i], l2[i+1] = l2[i+1], l2[i]
-                res.append([2, i+1])
-
-    print(l1)
-    print(l2)
     print(len(res))
     for l in res:
         print(" ".join(map(str,l)))
---
+++ D_Phoenix_and_Socks.py
@@ -0,0 +1,57 @@
+from collections import Counter
+for _ in range(int(input())):
+    n, l, r = map(int, input().split())
+    nums = list(map(int, input().split()))
+    m = min(l,r)
+    
+    left_dict = Counter(nums[:l])
+    right_dict = Counter(nums[l:])
+    
+    for k in nums[l:]:
+        if k in left_dict:
+            mn_val = min(left_dict[k], right_dict[k])
+            left_dict[k] -= mn_val
+            right_dict[k] -= mn_val
+            l -= mn_val
+            if not left_dict[k]:
+                del left_dict[k]
+                
+            if not right_dict[k]:
+                del right_dict[k]
+            r -= mn_val
+    move = 0
+    # print(l, r)
+    if l < r:
+        dis, total_val = 0, 0
+        for k, v in right_dict.items():
+            dis = v%2
+            total_val += v-dis
+        # print(dis, total_val)
+        for i in range((r-l)//2):
+            if total_val:
+                total_val -= 2
+                move += 1
+            else:
+                dis -= 1
+                move += 2
+        move += l
+            
+        
+    elif r < l:
+            dis, total_val = 0, 0
+            for k, v in left_dict.items():
+                dis = v%2
+                total_val += v-dis
+            for i in range((l-r)//2):
+                if total_val:
+                    total_val -= 2
+                    move += 1
+                else:
+                    dis -= 1
+                    move += 2
+            move += r
+                        
+    else:
+        move += l            
+    
+    print(move)

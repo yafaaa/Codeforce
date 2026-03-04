51ffff29c1fd2125a9d35547050c487ade0b1c1f
yafaaa <yafetmichael36@gmail.com>
2026-03-04 19:37:49 +0300

    flip the bit

--- B_Flip_the_Bits.py
+++ B_Flip_the_Bits.py
@@ -1,7 +1,25 @@
 
-for _ in range(int(input())):
+def fun():
     n = int(input())
     nums = list(map(int, input()))
-    a = 0
-    for b in range(n):
-        ...
+    target = list(map(int, input()))
+    cnt0, cnt1 = 0, 0
+
+    for i in range(n-1):
+        if nums[i] == 1:
+            cnt1 += 1
+        else:
+            cnt0 += 1
+        
+        curr = (nums[i] == target[i])
+        nxt = (nums[i+1] == target[i+1]) 
+        if curr != nxt and cnt0 != cnt1:
+                return f"NO"
+    cnt0 += 1 if not nums[-1] else 0
+    if (nums[-1]!= target[-1]) and (cnt0 != n-cnt0):
+        return f"NO"
+    return f"YES"
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        print(fun())
---
+++ C_New_Year_and_Domino.py
@@ -0,0 +1,9 @@
+n, m = map(int, input())
+mat = []
+for _ in range(n):
+    mat.append(list(input().split()))
+
+# row1, row2, col1, col2 = 0, 0, 0, 0
+pre_ver = [[0]*(m+1) for i in range(n+1)]
+for pre_ver in 
+

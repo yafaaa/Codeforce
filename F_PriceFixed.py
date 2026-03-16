4cfa73f2f49ffb51a18e05fec3df808fc79a40ac
yafaaa <yafetmichael36@gmail.com>
2026-03-16 17:16:54 +0300

    f upsolve

--- F_PriceFixed.py
+++ F_PriceFixed.py
@@ -1,35 +1,32 @@
-from collections import deque
-nums = []
+import sys
+input = sys.stdin.readline
+
 n = int(input())
+nums = []
 for _ in range(n):
     nums.append(list(map(int, input().split())))
 
 nums.sort(key=lambda x: x[1])
 
 left = 0
+right = n - 1
 k = 0
 cost = 0
-right = n-1
-while right > -1:
-    a = nums[left]
-    b = nums[right]
 
-    while b[0] and a[1]> k:
-        b[0] -= 1
-        cost += 2
-        k += 1
-    if not b[0] and right > left:
-        right -= 1
-    while a[1]<=k:
-        cost += a[0]
-        if left+1 < n:
-            left += 1
-            a = nums[left]
-        else:
-            break
-    
-    
-print(cost)
-
-
+while left <= right:
+    if k >= nums[left][1]:
         
+        cost += nums[left][0]
+        k += nums[left][0]        
+        left += 1
+    else:
+        
+        batch = min(nums[right][0], nums[left][1] - k)  
+        cost += 2 * batch
+        k += batch
+        nums[right][0] -= batch
+        if nums[right][0] == 0:
+            right -= 1
+        
+
+print(cost)
